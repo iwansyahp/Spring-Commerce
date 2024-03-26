85662035e9d0e511e9ffa2468aadf1c175aa71bb
Iwansyah Putra <iwansyah.putra@jublia.com>
2024-03-27 02:04:43 +0700

    chore: tidy up controller for product

--- product-service/src/main/java/com/springcommerce/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/springcommerce/productservice/controller/ProductController.java
@@ -13,59 +13,60 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcommerce.productservice.dto.CreateUpdateProductRequest;
 import com.springcommerce.productservice.dto.PaginatedResponse;
+import com.springcommerce.productservice.dto.ProductResponse;
 import com.springcommerce.productservice.dto.Response;
 import com.springcommerce.productservice.entity.Product;
 import com.springcommerce.productservice.service.ProductService;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
-	
+
 	@Autowired
 	ProductService productService;
-	
-	@GetMapping("/products/active")
+
+	@GetMapping("/active")
 	public ResponseEntity<PaginatedResponse<Product>> getProducts(
 			@RequestParam(name="page", defaultValue = "1") int page,
 			@RequestParam(name="size", defaultValue = "5") int size) {
-		
+
 		try {
 			Page<Product> products = productService.findAllByActiveOrderByNameAsc(true, PageRequest.of(page, size));
-			
+
 			PaginatedResponse<Product> response = new PaginatedResponse<>(
 					products.getNumber(),
 					products.getTotalPages(),
 					products.getTotalElements(),
 					products.getContent()
 			);
-			
 			return ResponseEntity.ok(response);
-			
+
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@PutMapping("/products/{uuid}")
-	public ResponseEntity<Response<Product>> updateProduct(@RequestBody CreateUpdateProductRequest request, @PathVariable UUID uuid) {
-		ResponseEntity<Response<Product>> response = productService.update(uuid, request);
+
+	@PutMapping("/{uuid}")
+	public ResponseEntity<Response<ProductResponse>> updateProduct(@RequestBody CreateUpdateProductRequest request, @PathVariable UUID uuid) {
+		var response = productService.update(uuid, request);
 		return response;
 	}
-	
-	@DeleteMapping("/products/{uuid}")
-	public ResponseEntity<Response<Product>> updateProduct(@PathVariable UUID uuid) {
-		ResponseEntity response = productService.deleteProduct(uuid);
+
+	@DeleteMapping("/{uuid}")
+	public ResponseEntity<Response<ProductResponse>> deleteProduct(@PathVariable UUID uuid) {
+		var response = productService.deleteProduct(uuid);
 		return response;
 	}
-	
-	@PostMapping("/products")
+
+	@PostMapping
 	public ResponseEntity<Response<Product>> createProduct(@RequestBody CreateUpdateProductRequest request) {
-		ResponseEntity<Response<Product>> response = productService.create(request);
+		var response = productService.create(request);
 		return response;
 	}
-	
 }
