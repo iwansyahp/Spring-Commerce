dc322bd4c0bdfabb208e22d53a769a7a7fbebda3
Iwansyah Putra <iwansyah.putra@jublia.com>
2024-03-27 22:05:01 +0700

    chore: clean file

--- report-service/src/main/java/com/springcommerce/reportservice/kafka/KafkaConstants.java
+++ report-service/src/main/java/com/springcommerce/reportservice/kafka/KafkaConstants.java
@@ -1,11 +1,11 @@
 package com.springcommerce.reportservice.kafka;
 
-public class KafkaConstants {	
+public class KafkaConstants {
 	public static final String ORDER_TOPIC = "order-topic";
 	public static final String ORDER_CHECK_OUT = "order_checkout";
 	public static final String ORDER_PAY = "order_paid";
 	public static final String ORDER_CANCEL = "order_cancelled";
-	
+
 	public static final String GROUP_ID = "report-service-group";
 
 }
