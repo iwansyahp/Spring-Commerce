e6256798b499fde9b34fb2d9e991fc2ee6047bc7
Iwansyah Putra <iwansyah.putra@jublia.com>
2024-03-27 22:06:59 +0700

    feat: prepare DTO utils and sql schema

--- user-service/src/main/java/com/springcommerce/userservice/util/UserUtils.java
+++ user-service/src/main/java/com/springcommerce/userservice/util/UserUtils.java
@@ -1,5 +1,6 @@
 package com.springcommerce.userservice.util;
 
+import com.springcommerce.events.payload.KafkaUser;
 import com.springcommerce.userservice.dto.UserDTO;
 import com.springcommerce.userservice.entity.User;
 
@@ -10,5 +11,9 @@ public class UserUtils {
 
     public static UserDTO toUser(User user) {
         return new UserDTO(user.getEmail(), user.getFirstName(), user.getLastName());
+    }
+
+    public static KafkaUser toKafkaUser(User user) {
+        return new KafkaUser(user.getId(), user.getEmail(), user.getFirstName(), user.getLastName());
     }
 }
